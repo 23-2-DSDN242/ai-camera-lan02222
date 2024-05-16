8dd814d93b26ad1fab052024348c5c7227943d63
lan02222 <161381436+lan02222@users.noreply.github.com>
2024-05-16 14:12:48 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_a.jpg";
+let maskFile   = "mask_a.png";
+let outputFile = "output_a.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -24,47 +24,59 @@ function setup () {
 }
 
 function draw () {
+  let angle = 45;
+  let offsetX = 60;
+  let offsetY = 50;
+
   for(let i=0;i<4000;i++) {
     //random xy
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
+    
+    // push();
+    // rotate(radians(angle));
+        
+    
     fill(pix);
   //   //draw on mask
-  //   if(mask[0] > 128) {
-  //     let pointSize = 10;
-  //     ellipse(x, y, pointSize, pointSize);
-  //   }
-  //   //draw on input
-  //   else {
-  //     fill(0, pix[1], 0);
-  //     let pointSize = 20;
-  //     rect(x, y, pointSize, pointSize);    
-  //   }
-  // }
+    if(mask[0] > 128) {
+      let pointSize = 10;
+      ellipse(x, y, pointSize, pointSize);
+    }
+    //draw on input
+    else {
+      fill(0, pix[1], 0);
+      let pointSize = 20;
+      rect(x, y, pointSize, pointSize);    
+    }
+  }
 
   //example
-  if (mask[0] > 128) {
-    let pointSize = 10;
-  //different stroke color
-    stroke(pix)
-    strokeWeight(height/500)
-    console.log(pix[0])
-    if(pix[0] > 120){
-    line(x, y, x, y+height/10)
-    }else{
-      line(x, y, x, y-height/10)
-    }
+  // if (mask[0] > 128) {
+  //   let pointSize = 10;
+  // //different stroke color
+  //   stroke(pix)
+  //   strokeWeight(height/500)
+  //   console.log(pix[0])
+  //   if(pix[0] > 120){
+  //   line(x+offsetX, y+offsetY, x+offsetX, y+height/10+offsetY)
+  //   }else{
+  //     line(x+offsetX, y+offsetY, x+offsetX, y-height/10+offsetY)
+  //   }
 
-  }
-  else{
-    noStroke()
-    fill(0,pix[1],0);
-    let pointSize = 20;
-    rect(x,y,pointSize, pointSize);
-  }
-  }
+  //   pop();
+
+
+  // }
+  // else{
+  //   noStroke()
+  //   fill(0,pix[1],0);
+  //   let pointSize = 20;
+  //   rect(x,y,pointSize, pointSize);
+  // }
+  // }
 
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
