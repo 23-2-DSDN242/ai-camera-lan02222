8c304b9cf1af6b097eee23e3d103ebc2e96fccd9
lan02222 <161381436+lan02222@users.noreply.github.com>
2024-06-02 14:06:07 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 let maskCenter = null;
 let maskCenterSize = null;
 
@@ -105,6 +105,7 @@ function draw () {
     noLoop();
     // uncomment this to save the result
     // saveArtworkImage(outputFile);
+
   }
 }
 
